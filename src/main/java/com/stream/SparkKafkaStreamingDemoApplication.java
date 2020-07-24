3bf774c43a2e3058f88b13d64a5b566291fb8b6e
pallavi-shetty <pallavis.ttl@tatamotors.com>
2020-07-24 18:04:15 +0530

    first commit

---
+++ src/main/java/com/stream/SparkKafkaStreamingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkKafkaStreamingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkKafkaStreamingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stream/SparkKafkaStreamingDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.stream;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkKafkaStreamingDemoApplicationTests {
+
+	@Ignore
+	@Test
+	void contextLoads() {
+	}
+
+}
